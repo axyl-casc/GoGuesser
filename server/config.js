1f12ee59668c79f23c7a73f50781383b7ecd13e3
Axyl S <axylschulz0@gmail.com>
2026-01-13 15:00:56 -0700

    Increase wait interval between rounds

--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
     SGF_INTERVAL: 30, // seconds between SGF changes
-    WAIT_INTERVAL: 10, // seconds between rounds
+    WAIT_INTERVAL: 15, // seconds between rounds
     SGF_DIR: path.join(__dirname, '..', 'sgf')
 };
